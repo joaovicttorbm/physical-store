cf31c55729086ba3d7e88c172b985af7c6192acf
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-04-14 15:59:47 -0300

    update responde dto

--- src/store/dto/delivery-option.dto.ts
+++ src/store/dto/delivery-option.dto.ts
@@ -4,12 +4,10 @@ export class DeliveryOptionDto {
   @ApiProperty({ description: 'Prazo de entrega', example: '2 dias úteis' })
   prazo: string;
 
-  @ApiProperty({ description: 'Código do produto da agência', example: '04014' })
-  codProdutoAgencia: string;
+  @ApiProperty({ description: 'Agência', example: 'xxxx' })
+  Agencia: string;
 
   @ApiProperty({ description: 'Preço do frete', example: 'R$ 27,00' })
   price: string;
 
-  @ApiProperty({ description: 'Descrição do serviço', example: 'Sedex a encomenda expressa dos Correios' })
-  description: string;
 }
